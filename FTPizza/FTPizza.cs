97c1742ff837746005d066196c0f2c888de78656
Kevin Bui <bui.kevin@live.com>
2016-06-30 15:12:07 -0700

    Setup basic user input to connect to FTP Server

--- FTPizza/FTPizza.cs
+++ FTPizza/FTPizza.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,17 @@ namespace FTPizza
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Enter the url for the FTP Server:");
+            string ftpUrl = Console.ReadLine();
+
+            Console.WriteLine("Enter the username for the FTP Server:");
+            string ftpUsername = Console.ReadLine();
+
+            Console.WriteLine("Enter the password for the FTP Server:");
+            string ftpPassword = Console.ReadLine();
+
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://" + ftpUrl);
         }
     }
 }
