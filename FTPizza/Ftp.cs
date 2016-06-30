18211770ba7d64c9462ef8a516b0eb596781bff6
Kevin Bui <bui.kevin@live.com>
2016-06-30 15:58:17 -0700

    Generated the Ftp class

---
+++ FTPizza/Ftp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FTPizza
+{
+    public class Ftp
+    {
+
+        public string _userName { get; set; }
+        public string _userPass { get; set; }
+        public string _userUrl  { get; set; }
+
+        public Ftp(string userName, string userPass, string userUrl)
+        {
+            _userName = userName;
+            _userPass = userPass;
+            _userUrl = userUrl;
+        }
+
+        internal void list()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void get()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void put()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void quit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
