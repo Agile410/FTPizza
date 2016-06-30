e542a21031d67b6d7b1a2faa0224f03f5f3d1eec
Kevin Bui <bui.kevin@live.com>
2016-06-29 18:07:52 -0700

    Created test project

---
+++ FTPizza.Tests/FTP.Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FTPizza.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- FTPizza/FTPizza.cs
+++ FTPizza/FTPizza.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FTPizza
 {
-    class Program
+    class FTPizza
     {
         static void Main(string[] args)
         {
