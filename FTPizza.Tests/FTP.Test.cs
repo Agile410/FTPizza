96b7fc7277ae3088efa5c94a9f19cc638b97a73f
Kevin Bui <bui.kevin@live.com>
2016-07-29 18:44:08 -0700

    First set of uni tests

--- FTPizza.Tests/FTP.Test.cs
+++ FTPizza.Tests/FTP.Test.cs
@@ -1,14 +1,25 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace FTPizza.Tests
 {
     [TestFixture]
-    public class UnitTest1
+    public class FTPTests
     {
         [Test]
-        public void TestMethod1()
+        public void LoginToFtpWithBadLogin()
         {
+            //Needs a more specific exception
+            Assert.That(() => new Ftp(null, null, null), Throws.Exception);
         }
+
+        [Test]
+        public void SuccessfulLoginIntoFtpServer()
+        {
+            var client = new Ftp("", "", "speedtest.tele2.net/");
+
+            Assert.That(client, Is.Not.Null);
+        }
+
     }
 }
