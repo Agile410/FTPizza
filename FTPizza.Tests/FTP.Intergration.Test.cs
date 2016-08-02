c1b67b5aedf5885efb38e23561c4741f629a6feb
Kevin Bui <bui.kevin@live.com>
2016-08-02 12:02:07 -0700

    Created a new file to host integration tests

---
+++ FTPizza.Tests/FTP.Intergration.Test.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+namespace FTPizza.Tests
+{
+    [TestFixture]
+    public class FTPIntegration
+    {
+        Ftp client;
+
+        [SetUp]
+        public void Setup()
+        {
+            var dir = System.IO.Path.GetDirectoryName(typeof(FTPIntegration).Assembly.Location);
+            System.Environment.CurrentDirectory = dir;
+            client = new Ftp("", "", "speedtest.tele2.net/");
+        }
+
+        [Test]
+        public void ShouldDownloadAFIle()
+        {
+            //Arrange
+            var testRequestedFiles = new List<string> { "512KB.zip" };
+
+            //Act
+            client.DownloadUsingFtp(testRequestedFiles);
+
+            //Assert
+            Assert.IsTrue(System.IO.File.Exists("512KB.zip"));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            System.IO.File.Delete("512KB.zip");
+        }
+    }
+}
